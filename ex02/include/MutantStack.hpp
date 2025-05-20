df0df0b8069155674a7f74dd6b3ee77781c4d140
Nikos <sarmadakis.nikos@gmail.com>
2025-05-20 12:16:58 +0200

    I might have to look into inheritance for 08/ex02

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -11,14 +11,51 @@ class MutantStack {
 	private:
 		std::deque<T> deque_;
 	public:
-		MutantStack();
-		~MutantStack();
-		MutantStack(const MutantStack& other);
-		MutantStack& (const MutantStack& other);
+		MutantStack() {};
+		~MutantStack() {};
+		MutantStack(const MutantStack& other) : deque_(other.deque_) {};
+		MutantStack& operator=(const MutantStack& other) {
+			if (this != &other) {
+				deque_ = other.deque_;
+			}
+			return (*this);
+		};
 
-		void push(const T& value);
-		void pop();
-		void top();
-		void size();
-		bool empty();
+		void	push(const T& value) {
+			deque_.push_back(value);
+		};
+		void	pop() {
+			deque_.pop_back();
+		};
+		T&		top() {
+			return (deque_.back());
+		};
+		const T& top() const {
+			return (deque_.back());
+		}
+		size_t	size() {
+			return (deque_.size());
+		};
+		bool	empty() {
+			return (deque_.empty());
+		};
+		
+		typedef typename std::deque<T>::iterator iterator;
+		typedef typename std::deque<T>::const_iterator constIterator;
+
+		iterator begin() noexcept {
+			return (deque_.begin());
+		};
+
+		constIterator begin() const noexcept {
+			return (deque_.cbegin());
+		};
+
+		iterator end() noexcept {
+			return (deque_.end());
+		};
+
+		constIterator end() const noexcept {
+			return (deque_.cend());
+		};
 };
