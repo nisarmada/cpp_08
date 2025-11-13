3cd0d22fe6ff5cd5d8e531c0a33d7b39cc9a8b17
Nikolaos Sarmadakis <sarmadakis.nikos@gmail.com>
2025-11-13 12:35:04 +0100

    added out of bounds test

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -11,24 +11,12 @@ int main() {
 		
 		std::cout << "Shortest span is " << span.shortestSpan() << std::endl;
 		std::cout << "Longest span is " << span.longestSpan() << std::endl;
+
+		span.addMultiple(array, array + 11); //this will fail cause it goes out of bounds
+		
 	}
 	catch (const std::out_of_range& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 	}
-	//test with random 15000 numbers
-	std::vector<int> test(15000);
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> distrib(-4500, 15000);
-	std::generate(test.begin(), test.end(), [&]() {return distrib(gen) ; });
-
-	Span randomSpan(15000);
-	try {
-		randomSpan.addMultiple(test.begin(), test.end());
-		std::cout << "Span filled with 15,000 random numbers." << std::endl;
-        std::cout << "Shortest span: " << randomSpan.shortestSpan() << std::endl;
-        std::cout << "Longest span: " << randomSpan.longestSpan() << std::endl;
-    } catch (std::exception& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
+	return 0;
 }
